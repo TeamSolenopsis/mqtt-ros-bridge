6468229edf9a2c6b8d00622f64f819fcd36acdc9
CasLeeflang <466933@student.fontys.nl>
2023-06-09 15:15:28 +0200

    t

--- mqtt_ros_brdg/mqtt_bridge/bridge.py
+++ mqtt_ros_brdg/mqtt_bridge/bridge.py
@@ -39,6 +39,7 @@ class Bridge(Node):
 
     def mqtt_check_connection(self):
         print('checking connection')
+        print(self.client.is_connected())
         if not self.client.is_connected():
             print('reconnecting')
             self.current_cmd_vel = Twist()
